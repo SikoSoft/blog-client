bc5125ef61d68aa09224c104b51f762361fd6969
Aaron Wright <aaron@sikosoft.com>
2020-01-09 07:41:04 +0100

    set publicPath to / for both environments

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,3 @@
-const config = require("./src/data/config.json");
-
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/" : "http://localhost:8080/"
+  publicPath: "/"
 };
