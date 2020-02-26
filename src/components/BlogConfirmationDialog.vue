534d27873f6e10fe0ce60ea414a8fe0fc120ed3d
Aaron Wright <aaron@sikosoft.com>
2020-02-26 21:00:49 +0100

    BlogConfirmationDialog component created

---
+++ src/components/BlogConfirmationDialog.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="blog-confirmation-dialog" :class="{ 'blog-confirmation-dialog--open': isOpen }">
+    <h3 class="blog-confirmation-dialog__title">{{ title }}</h3>
+    <div class="blog-confirmation-dialog__message">{{ message }}</div>
+    <div class="blog-confirmation-dialog__buttons">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "blog-confirmation-dialog",
+
+  props: ["title", "message", "isOpen"]
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/variables.scss";
+
+.blog-confirmation-dialog {
+  display: none;
+  position: fixed;
+  width: 30vw;
+  height: 30vh;
+  border-radius: 8px;
+
+  &--open {
+    display: block;
+  }
+
+  .blog-confirmation-dialog__title {
+    font-size: $font-large;
+    color: $color-emphasis;
+  }
+
+  .blog-confirmation-dialog__message {
+    font-size: $font;
+    color: $color-text-main;
+  }
+
+  .blog-confirmation-dialog__buttons {
+    padding: $space-large;
+    text-align: center;
+  }
+}
+</style>
