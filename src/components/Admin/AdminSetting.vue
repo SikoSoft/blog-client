07b02782af1cc36d31be16471047f72035c0fc8a
Aaron Wright <aaron@sikosoft.com>
2020-02-26 21:02:20 +0100

    AdminSetting(s) components created

---
+++ src/components/Admin/AdminSetting.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="admin-setting"></div>
+</template>
+
+<script>
+export default {
+  name: "admin-setting",
+
+  props: [],
+
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Admin/AdminSettings.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="admin-settings"></div>
+</template>
+
+<script>
+export default {
+  name: "admin-settings"
+};
+</script>
+
+<style lang="scss" scoped>
+.admin-settings {
+}
+</style>
