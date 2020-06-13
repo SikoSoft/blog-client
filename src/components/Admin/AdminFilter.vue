cce5b3088b5156a7dadf710d18bde91c04eec753
Aaron Wright <aaron@sikosoft.com>
2020-06-13 23:52:03 +0200

    AdminFilter component created

---
+++ src/components/Admin/AdminFilter.vue
@@ -0,0 +1,65 @@
+<template>
+  <li class="admin-filter">
+    <div class="admin-filter__col">
+      <input
+        class="admin-filter__input"
+        :placeholder="$strings.id"
+        type="text"
+        :value="id"
+        @keydown="handleUpdate"
+      />
+    </div>
+    <div class="admin-filter__col">
+      <input
+        class="admin-filter__input"
+        :placeholder="$strings.title"
+        type="text"
+        :value="label"
+        @keydown="handleUpdate"
+      />
+    </div>
+    <div class="admin-filter__col">
+      <input class="admin-filter__input" :placeholder="$strings.image" type="text" :value="image" />
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "admin-filter",
+
+  props: ["id", "label", "image"],
+
+  methods: {
+    handleUpdate() {}
+  }
+};
+</script>
+
+<style lang="scss">
+@import "@/styles/variables.scss";
+
+.admin-filter {
+  margin: $space-small 0;
+  display: flex;
+  &__col {
+    padding: $space-xxxsmall;
+
+    &:first-child,
+    &:nth-child(2) {
+      flex: 1;
+    }
+    &:last-child {
+      flex: 2;
+    }
+  }
+  &__input {
+    width: 100%;
+    box-sizing: border-box;
+    border: 2px transparent solid;
+    &.admin-filters__filter-input--unsaved {
+      background-color: $color-action-neutral-bg;
+    }
+  }
+}
+</style>
--- src/components/Admin/AdminFilters.vue
+++ src/components/Admin/AdminFilters.vue
@@ -1,11 +1,44 @@
 <template>
-  <div class="admin-filters"></div>
+  <div class="admin-filters">
+    <ul class="admin-filters__list">
+      <admin-filter v-for="filter in filters" :key="filter.id" v-bind="filter" />
+    </ul>
+  </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from "vuex";
+
+import AdminFilter from "@/components/Admin/AdminFilter.vue";
+
 export default {
-  name: "filters"
+  name: "filters",
+
+  components: { AdminFilter },
+
+  computed: {
+    ...mapGetters(["filters"])
+  },
+
+  methods: {
+    ...mapActions(["getFilters"])
+  },
+
+  mounted() {
+    this.getFilters().then(() => {});
+  }
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import "@/styles/variables.scss";
+
+.admin-filters {
+  &__list {
+    width: 100%;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+}
+</style>
