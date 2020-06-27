9bb32bcfdabc6a0d3ffd20ad110056b91a9e3ba8
Aaron Wright <aaron@sikosoft.com>
2020-06-27 23:13:17 +0200

    added settings for controlling further reading

---
+++ src/components/BlogFurtherReading.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="blog-further-reading"></div>
+</template>
+
+<script>
+export default {
+  name: "blog-further-reading",
+
+  props: []
+};
+</script>
+
+<style lang="scss" scoped>
+.blog-further-reading {
+}
+</style>
