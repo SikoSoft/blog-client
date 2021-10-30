f0ffedc38c761f28cb364a9dda5004d4dc8383e1
Aaron Wright <aaron@sikosoft.com>
2021-10-30 21:55:34 +0200

    added role configuration to settings interface

--- src/components/Admin/AdminSetting.vue
+++ src/components/Admin/AdminSetting.vue
@@ -2,8 +2,24 @@
   <div class="admin-setting">
     <div class="admin-setting__label">{{ label }}</div>
     <div class="admin-setting__input">
+      <template v-if="type === 'list'">
+        <select v-model="value">
+          <option>{{ $strings.selectARole }}</option>
+          <option
+            v-for="option in listOptions(listSource)"
+            :key="option.value"
+            :value="option.value"
+            :selected="option.value === value"
+            >{{ option.label }}</option
+          >
+        </select>
+      </template>
       <template v-if="type === 'number'">
-        <input type="text" class="admin-setting__input-number" v-model="value" />
+        <input
+          type="text"
+          class="admin-setting__input-number"
+          v-model="value"
+        />
       </template>
       <template v-if="type === 'text'">
         <input type="text" class="admin-setting__input-text" v-model="value" />
@@ -16,7 +32,7 @@
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 
 import BlogToggle from "@/components/BlogToggle.vue";
 
@@ -30,6 +46,7 @@ export default {
       type: String,
       default: "toggle"
     },
+    listSource: { type: String, default: "" },
     maxLength: { type: Number, default: 3 },
     initialValue: { type: [String, Number, Boolean] }
   },
@@ -40,6 +57,10 @@ export default {
     return {
       value: this.initialValue ? this.initialValue : ""
     };
+  },
+
+  computed: {
+    ...mapGetters({ listOptions: "settingListOptions" })
   },
 
   methods: {
