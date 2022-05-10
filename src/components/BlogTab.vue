2937f9125e43758cd8a454d9967d6099a3e34110
Aaron Wright <aaron@sikosoft.com>
2022-05-10 18:46:24 +0200

    added BlogTab and BlogTabs components to handle tabbed UI

---
+++ src/components/BlogTab.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="blog-tab" :class="{ 'blog-tab--active': isActive }">
+    <slot />
+  </section>
+</template>
+
+<script>
+export default {
+  name: "blog-tab",
+
+  props: {
+    title: {
+      type: String
+    }
+  },
+
+  data() {
+    return {
+      isActive: false
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+.blog-tab {
+  height: 0;
+  overflow: hidden;
+  transition: all 0.3s;
+
+  &--active {
+    height: auto;
+  }
+}
+</style>
